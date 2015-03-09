c7a7d10f727b52cfa78951c11ffac91d3539dd03
githubgabriel <tx.gabrielbarbosa@gmail.com>
2015-03-09 10:25:16 -0400

    Index e CSS
    Tabela e CSS criados!

---
+++ index.php
@@ -0,0 +1,53 @@
+<?
+
+?>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>KeyPass</title>
+    <link href="css/layout.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+
+<div id="conteudo">
+
+    <table border="0" id="registros">
+        <thead>
+            <tr>
+                <td> Categoria </td>
+                <td> Tipo </td>
+                <td> Website </td>
+                <td> URL </td>
+                <td> Login </td>
+                <td> Senha </td>
+                <td> Descrição </td>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td> Categoria </td>
+                <td> Tipo </td>
+                <td> Website </td>
+                <td> URL </td>
+                <td> Login </td>
+                <td id="pass"> Senha </td>
+                <td> Descrição </td>
+            </tr>
+            <tr>
+                <td> Categoria </td>
+                <td> Tipo </td>
+                <td> Website </td>
+                <td> URL </td>
+                <td> Login </td>
+                <td id="pass"> Senha </td>
+                <td> Descrição </td>
+            </tr>
+        </tbody>
+
+    </table>
+
+</div>
+
+</body>
+</html>
